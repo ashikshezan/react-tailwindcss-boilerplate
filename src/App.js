58812761ad34f7628241769363aa8898d220e9a8
ashikshezan <ashikshezan@gmail.com>
2021-01-09 20:25:37 +0600

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div>
-      Starts here ...
+      <div className={"m-10 h-16 w-16 bg-gray-500"}></div>
+      <div className={"m-10 h-16 w-16 bg-gray-900 rounded-full"}></div>
     </div>
   );
 }
